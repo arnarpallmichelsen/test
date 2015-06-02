503dbf6b4c4d7c1d4e7dec8a1b0903d2bf3b3cdc
unknown <amichels@NOLAMICHELS02.one.steria.dom>
2015-06-02 16:01:44 +0200

    lagt til ny fil

---
+++ adresse.sql
@@ -0,0 +1,14 @@
+﻿-- Table: dev_test.adresse
+
+-- DROP TABLE dev_test.adresse;
+
+CREATE TABLE dev_test.adresse
+(
+  adresse character varying(50),
+  postnummer character varying(4)
+)
+WITH (
+  OIDS=TRUE
+);
+ALTER TABLE dev_test.adresse
+  OWNER TO postgres;
